966390c4955994c0faf39eaf16b8a057b743cfff
heath mcburnett <heath@therevolution.com>
2013-05-13 18:39:38 -0500

    Created Rails app added status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RevOnRails::Application.config.secret_token = '6e9bb29367c942dee5e6704c37e2d70cb1a1414e9b1a21aad651726446a8ac9f546774de1eb8488ac65b578b16d3a704375dafef588195546b8a9d71c17f2f5e'
